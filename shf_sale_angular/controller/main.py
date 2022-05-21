c808408a390aa93e0ba0a49338f8b2a76bb1ea4d
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-05-20 19:10:59 -0500

    :D

--- shf_sale_angular/controller/main.py
+++ shf_sale_angular/controller/main.py
@@ -4,6 +4,7 @@ import logging
 import pprint
 _logger = logging.getLogger(__name__)
 from urllib.parse import urlparse
+import json
 
 
 class WebHooks(http.Controller):
@@ -14,15 +15,61 @@ class WebHooks(http.Controller):
         data = {
             'data': {
                 'token': str(request.session.session_token) ,
-                'id_sale':  instance.id
+                'id_sale':  instance.id ,
+                'url': self.env['ir.config_parameter'].search([('key','=','web.base.url')]).value ,
+                'tag_read' : 'read_sale_angular/'+str(instance.id) ,
+                'tag_write' :  'write_sale_angular/'+str(instance.id) ,
+
             }
 
         }
 
         return http.request.render('shf_sale_angular.sale_index_angular',data)
-        #data = http.request.jsonrequest
-        #_logger.info('creacion del producto %s', pprint.pformat(data))
-        #http.request.env['woo.synchro'].sudo().hook(data,instance,'products')
 
+    @http.route(['/read_sale_angular/<model("sale.order"):sale>'], type='http', auth="public", methods=['GET'], website=True, csrf=False)
+    def index2(self, sale ,**kw):
+
+        dx = {
+            'id_sale': sale.id ,
+            'name': sale.name ,
+            'partner': sale.partner_id.name  ,
+            'items': []
+        }
+
+        for l in sale.order_line:
+
+
+            dx['items'].append(
+
+                {
+                    "id": l.id ,
+                    "parentId": l.parent_section_id.id ,
+                    #"selected": false,
+                    "consecutive": str(l.sequence) ,
+                    "data": {
+                        "id": l.id ,
+                        "type": "GROUP",
+                        "description": "",
+                        "name": "",
+                        "brand": "",
+                        "unitOfMeasurement": 0,
+                        "taxes": [
+                            {
+                                "id": "1",
+                                "name": "",
+                                "value": 0
+                            }
+                        ],
+                        "amount": 0,
+                        "listPrice": 0,
+                        "discountPercentage": 0,
+                        "gainPercentage": 0,
+                        "cost": 0,
+                        "stock": 0 ,
+                    }
+                }
+            )
+
+        return json.dumps(dx)
 
 
